f7952acf632aa6013e215bc115889d606c4cd538
11ioo <11ioo@napalt.co.kr>
2024-10-28 15:05:26 +0900

    예외처리 업데이트

--- src/main/java/com/example/schedule3/controller/GlobalExceptionHandler.java
+++ src/main/java/com/example/schedule3/controller/GlobalExceptionHandler.java
@@ -1,15 +1,22 @@
 package com.example.schedule3.controller;
 
+import jakarta.validation.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<Map<String, String>> handleGenericException(Exception ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("errorCode", "ERR500");
+        errorResponse.put("errorMessage", "네트워크 요청에 실패했습니다. 다시 시도해주시기 바랍니다.");
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
